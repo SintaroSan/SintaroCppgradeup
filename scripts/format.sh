9593eac70b204dee5fd501fc50fa8f09c7210ca6
SEIka69 <52436100+SEIka1@users.noreply.github.com>
2025-06-20 13:45:38 +0300

    Update format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-find members -name '*.cpp' -o -name '*.hpp' | xargs clang-format -i --style=file
-echo "Форматирование завершено"
+
+find members -type f \( -name "*.cpp" -o -name "*.hpp" \) -print0 | while IFS= read -r -d '' file; do
+    echo "Форматирование $file"
+    clang-format -i --style=file "$file"
+done
+
+echo "Все файлы отформатированы"
