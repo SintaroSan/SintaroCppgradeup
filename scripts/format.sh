f03cbc8908c45071d07637ead19f33477bb06e94
SEIka69 <52436100+SEIka1@users.noreply.github.com>
2025-06-20 14:03:12 +0300

    Delete scripts directory

--- scripts/format.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find members -type f \( -name "*.cpp" -o -name "*.hpp" \) -print0 | while IFS= read -r -d '' file; do
-    echo "Форматирование $file"
-    clang-format -i --style=file "$file"
-done
-
-echo "Все файлы отформатированы"
